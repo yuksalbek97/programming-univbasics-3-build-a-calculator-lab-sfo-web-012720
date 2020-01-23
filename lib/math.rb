55eefc05f929c0f3e77ab29b69245a3db7fcec50
honest-operand-5068 <yuksal97@gmail.com>
2020-01-23 00:57:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,11 +6,12 @@ addition(5,4)
 def subtraction(num1, num2)
 num1 - num2
 end
-subtraction
+subtraction(10,5)
 
 def division(num1, num2)
-
+num1 * num2 
 end
+division(50,2)
 
 def multiplication(num1, num2)
 
