499de6170aca89dc497a4a6d9ba9f1b699ae549f
honest-operand-5068 <yuksal97@gmail.com>
2020-01-23 01:04:03 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
+addition(5,4)
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
+subtraction(10,5)
 
 def division(num1, num2)
-
+num1 / num2 
 end
+division(50,2)
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
+multiplication(4,30)
 
 def modulo(num1, num2)
-
+num1 % num2
 end
+modulo(34,5)
 
 def square_root(num)
-
+Integer.sqrt(num)
 end
+square_root(81)
